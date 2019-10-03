5d83abd4a57df06b22d0822eb8e870eae987efed
CookieMonsta89 <jlstan06@hotmail.com>
2019-10-03 17:17:49 -0400

    font change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img style={{marginRight:'8%'}} src={tumbling}/>
+      <img src={tumbling}/>
         ACE GYMNASTICS!! COMING SOON!!
       </header>
     </div>
