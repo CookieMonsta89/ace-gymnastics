db2136113a851e0cd03da13356d05c7eda93e33e
CookieMonsta89 <jlstan06@hotmail.com>
2019-10-03 17:03:44 -0400

    loading screen for under construction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import tumbling from './assets/tumbling.gif';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={tumbling}/>
         ACE GYMNASTICS!! COMING SOON!!
       </header>
     </div>
