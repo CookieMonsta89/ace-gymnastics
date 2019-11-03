a882da53b9baea6ee577797b32ce25ec30e0032d
CookieMonsta89 <jlstan06@hotmail.com>
2019-11-03 17:56:47 -0500

    added open house and social media links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
-import tumbling from './assets/tumbling.gif';
+import React from "react";
+import "./App.css";
+import tumbling from "./assets/tumbling.gif";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {faFacebookSquare, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src={tumbling}/>
+        <img src={tumbling} />
         ACE GYMNASTICS!! COMING SOON!!
+        <div>
+          <h3>Open House:</h3>
+          <p>December 7th, 1pm - 3pm</p>
+        </div>
+        <div className='icon-logos'>
+          <a href='mailto: office@ace-gymnastics.com'><FontAwesomeIcon icon={faEnvelope} color='white' /></a>
+          <a href='https://www.facebook.com/ACE-Gymnastics-101806134601903/' target='_blank'><FontAwesomeIcon icon={faFacebookSquare} color='white' /></a>
+          <a href='https://www.instagram.com/ace.gymnastics/' target='_blank'><FontAwesomeIcon icon={faInstagram} color='white' /></a>
+        </div>
       </header>
     </div>
   );
