e9af837386a75a786424101bdaa77c495ab35ac9
CookieMonsta89 <jlstan06@hotmail.com>
2019-11-03 18:07:57 -0500

    phone added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import tumbling from "./assets/tumbling.gif";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
 import {faFacebookSquare, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 function App() {
@@ -16,9 +16,10 @@ function App() {
           <p>December 7th, 1pm - 3pm</p>
         </div>
         <div className='icon-logos'>
-          <a href='mailto: office@ace-gymnastics.com'><FontAwesomeIcon icon={faEnvelope} color='white' /></a>
-          <a href='https://www.facebook.com/ACE-Gymnastics-101806134601903/' target='_blank'><FontAwesomeIcon icon={faFacebookSquare} color='white' /></a>
-          <a href='https://www.instagram.com/ace.gymnastics/' target='_blank'><FontAwesomeIcon icon={faInstagram} color='white' /></a>
+          <a href='mailto: office@ace-gymnastics.com'><FontAwesomeIcon icon={faEnvelope} color='white' size='2x' /></a>
+          <a href='https://www.facebook.com/ACE-Gymnastics-101806134601903/' target='_blank'><FontAwesomeIcon icon={faFacebookSquare} color='white' size='2x' /></a>
+          <a href='https://www.instagram.com/ace.gymnastics/' target='_blank'><FontAwesomeIcon icon={faInstagram} color='white' size='2x' /></a>
+          <a href="tel:502-271-8934"><FontAwesomeIcon icon={faPhoneSquare} color='white' size='2x' /></a>
         </div>
       </header>
     </div>
