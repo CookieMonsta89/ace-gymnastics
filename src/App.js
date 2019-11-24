c6df1a080004d6de6d793c05f8f64f25c93a5c71
CookieMonsta89 <jlstan06@hotmail.com>
2019-11-23 19:03:46 -0500

    added new styling to website navigation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/header";
 import { Route } from "react-router-dom";
+import Home from "./containers/Home/Home";
 
 function App() {
-	console.log(window.screen.width, "window");
 	return (
 		<div className="App">
 			<Header />
-			<Route path="/" />
+			<Route exact path="/" component={Home} />
 			<Route path="/About" />
 		</div>
 	);
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,7 @@
 import React from "react";
-import placeholder from "../../assets/placeholder.gif";
+import logofull from "../../assets/logofull.png";
 import { Link } from "react-router-dom";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // <div className="contact-banner">
@@ -21,9 +21,19 @@ class Header extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			toggleNav: "link-tab"
+			toggleNav: "link-tab",
+			activeLink: "home"
 		};
 	}
+
+	componentDidUpdate = (prevProps, prevState) => {
+		console.log("updated", prevProps, prevState, this.state);
+		if (prevState.activeLink !== this.state.activeLink) {
+			this.setState({
+				toggleNav: "link-tab"
+			});
+		}
+	};
 
 	handleClickTab = () => {
 		const { toggleNav } = this.state;
@@ -32,27 +42,82 @@ class Header extends React.Component {
 		});
 	};
 
+	selectActiveLink = param => {
+		this.setState({
+			activeLink: param
+		});
+	};
+
+	activeObject = active => {
+		const { activeLink } = this.state;
+		let object = {
+			borderBottom: "3px solid white"
+		};
+		let object2 = {
+			borderBottom: "0px solid white",
+			background: "white",
+			color: "#9a0b0b",
+			transition: "all .5s"
+		};
+		if (activeLink === active && window.screen.width >= 768) {
+			return object;
+		}
+		if (activeLink === active && window.screen.width < 768) {
+			return object2;
+		}
+	};
+
 	render() {
 		const { toggleNav } = this.state;
+		console.log(window.screen.width);
 		return (
 			<div className="header-container">
 				<div className="link-container">
 					<span className="logo-container">
-						<img src={placeholder} alt="logo" />
+						<img src={logofull} alt="logo" />
 					</span>
 					<span className="hamburger" onClick={this.handleClickTab}>
-						<FontAwesomeIcon icon={faBars} color="white" size="2x" />
+						<FontAwesomeIcon
+							icon={toggleNav === "link-tab-open" ? faTimes : faBars}
+							color="white"
+							size="2x"
+						/>
 					</span>
 					<div className={toggleNav}>
-						<div className="navlink">
-							<Link className="link" to="/">
+						<div
+							className="navlink"
+							style={this.activeObject("home")}
+							onClick={() => this.selectActiveLink("home")}
+						>
+							<Link className="link" to="/" style={this.activeObject("home")}>
 								Home
 							</Link>
 						</div>
-						<div className="navlink">Team</div>
-						<div className="navlink">Link</div>
-						<div className="navlink">
-							<Link className="link" to="About">
+						<div
+							className="navlink"
+							style={this.activeObject("team")}
+							onClick={() => this.selectActiveLink("team")}
+						>
+							Team
+						</div>
+						<div
+							className="navlink"
+							style={this.activeObject("link")}
+							onClick={() => this.selectActiveLink("link")}
+						>
+							Link
+						</div>
+						<div
+							className="navlink"
+							name="about"
+							style={this.activeObject("about")}
+							onClick={() => this.selectActiveLink("about")}
+						>
+							<Link
+								className="link"
+								to="About"
+								style={this.activeObject("about")}
+							>
 								About
 							</Link>
 						</div>
