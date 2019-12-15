5f8c4e2db687f14e3e766ee710d4e1116092148c
CookieMonsta89 <jlstan06@hotmail.com>
2019-12-15 16:55:47 -0500

    added link to jackrabbit

--- src/components/Jackrabbit/jackrabbit.js
+++ src/components/Jackrabbit/jackrabbit.js
@@ -3,8 +3,18 @@ import React from "react";
 const Jackrabbit = () => {
 	return (
 		<div className="jackrabbit">
-			<button className="button">REGISTER</button>
-			<button className="button account-button">ACCOUNT</button>
+			<a
+				href="https://app.jackrabbitclass.com/regv2.asp?id=539177"
+				target="_blank"
+			>
+				<button className="button">REGISTER</button>
+			</a>
+			<a
+				href="https://app.jackrabbitclass.com/jr3.0/ParentPortal/Login?orgID=539177"
+				target="_blank"
+			>
+				<button className="button account-button">ACCOUNT</button>
+			</a>
 		</div>
 	);
 };
--- src/components/MidPageBreak/midPage.js
+++ src/components/MidPageBreak/midPage.js
@@ -4,7 +4,12 @@ const MidPageBreak = () => {
 	return (
 		<div className="midPage-container">
 			Already have an account! Access online portal here.
-			<button className="button account-button">Account</button>
+			<a
+				href="https://app.jackrabbitclass.com/jr3.0/ParentPortal/Login?orgID=539177"
+				target="_blank"
+			>
+				<button className="button account-button">Account</button>
+			</a>
 		</div>
 	);
 };
