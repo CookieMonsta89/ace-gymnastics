7b758e73d50e1fd58b0b8f34802d9844d238f027
CookieMonsta89 <jlstan06@hotmail.com>
2019-12-15 17:14:18 -0500

    mobile reg

--- src/components/Jackrabbit/jackrabbitMobile.js
+++ src/components/Jackrabbit/jackrabbitMobile.js
@@ -3,8 +3,18 @@ import React from "react";
 const JackrabbitMobile = () => {
 	return (
 		<div className="jackrabbit-mobile">
-			<button className="button">REGISTER</button>
-			<button className="button account-button">ACCOUNT</button>
+			<a
+				href="https://app.jackrabbitclass.com/regv2.asp?id=539177"
+				target="_blank"
+			>
+				<button className="button">REGISTER</button>
+			</a>
+			<a
+				href="https://app.jackrabbitclass.com/jr3.0/ParentPortal/Login?orgID=539177"
+				target="_blank"
+			>
+				<button className="button account-button">ACCOUNT</button>
+			</a>
 		</div>
 	);
 };
